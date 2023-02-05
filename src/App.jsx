83c9f3592f218edca2173d1cd382a863a48172c0
Gacus <36047400+Gacus@users.noreply.github.com>
2023-02-05 21:11:53 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center bg-gradient-to-bl from-cyan-500 to-blue-500">
+      <h1 className="text-2xl text-white">Vite React Tailwind</h1>
+    </div>
+  )
+}
+
+export default App
